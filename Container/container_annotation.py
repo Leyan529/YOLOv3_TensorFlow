727c2790f7381cbad4509d7277df631003d8ca48
DESKTOP-CPGNDAC\Leyan <afly.bsky@gmail.com>
2019-07-21 19:14:11 +0800

    no message

--- Container/container_annotation.py
+++ Container/container_annotation.py
@@ -3,7 +3,7 @@ import os
 from os import listdir, getcwd
 from os.path import join
 import shutil
-
+import numpy as np
 
 
 classes = ["aquarium","bottle","bowl","box","bucket","plastic_bag","plate",
@@ -29,23 +29,27 @@ def convert_annotation(path,image_id, train_file):
     width = int(root.find('size').findtext('width'))
     height = int(root.find('size').findtext('height'))
 
-    if root.find('object'):
-        train_file.write(path)     
-    for obj in root.iter('object'):
-        cls = obj.find('name').text
-        if cls not in classes:
-            continue
-        cls_id = int(classes.index(cls))
-        #xmlbox = obj.find('bndbox')
+    ehance_fn_list=["","_bri1","_bri2","_col1","_col2","_con1","_con2","_sha1","_sha2"]
+    for idx,d in enumerate(ehance_fn_list):        
+        if root.find('object'):
+            new_path = path.replace(".jpg",d+".jpg")
+            train_file.write(new_path)               
+        for obj in root.iter('object'):
+            cls = obj.find('name').text
+            if cls not in classes:
+                continue
+            cls_id = int(classes.index(cls))
+            #xmlbox = obj.find('bndbox')
 
-        xmin = obj.find('bndbox').findtext('xmin')
-        ymin = obj.find('bndbox').findtext('ymin')
-        xmax = obj.find('bndbox').findtext('xmax')
-        ymax = obj.find('bndbox').findtext('ymax')
-        b = [cls_id,xmin,ymin,xmax,ymax]       
-        
-        train_file.write(" " + " ".join([str(a) for a in b]))
-    train_file.write(" ")
+            xmin = obj.find('bndbox').findtext('xmin')
+            ymin = obj.find('bndbox').findtext('ymin')
+            xmax = obj.find('bndbox').findtext('xmax')
+            ymax = obj.find('bndbox').findtext('ymax')
+            b = [cls_id,xmin,ymin,xmax,ymax]       
+            
+            train_file.write(" " + " ".join([str(a) for a in b]))  
+        if idx != len(ehance_fn_list)-1 :
+            train_file.write("\n")
 
         # b = (float(xmin), float(xmax), float(ymin), float(ymax))
         # bb = convert(cls_id,(width, height), b)
@@ -68,7 +72,48 @@ def create_keras_data():
     train_file.close()
     print("Finish...")
 
+def data_split(split,file,fn1,fn2):
+    with open(file,"r") as f:
+        lines = f.readlines()        
+    np.random.seed(10101)
+    np.random.shuffle(lines)
+    np.random.seed(None)
+    num_val = int(len(lines)*split)
+    num_train = len(lines) - num_val
+    train_lines = lines[:num_train]
+    val_lines = lines[num_train:]
+
+    with open(fn1,"w") as f:
+        for idx,line in enumerate(train_lines):   
+            f.write(str(idx) + " ")
+            line = line.replace("\n","")     
+            f.write(line)
+            if idx != len(train_lines)-1:
+                f.write("\n")
+        f.flush()
+
+    with open(fn2,"w") as f:
+        for idx,line in enumerate(val_lines):
+            f.write(str(idx) + " ")
+            line = line.replace("\n","")   
+            f.write(line)
+            if idx != len(val_lines)-1:
+                f.write("\n")
+        f.flush()
+
+
+    return train_lines , val_lines
 
 if __name__ == '__main__':
+    ### Some paths
+    # train_file = './data/my_data/train.txt'  # The path of the training txt file.
+    # val_file = './data/my_data/val.txt'  # The path of the validation txt file.
+    train_file = "train.txt"  # The path of the training txt file.
+    val_file = "val.txt"  # The path of the validation txt file.
+    data_file = 'container.txt'  # The path of the validation txt file.
     create_keras_data()
 
+    val_split = 0.1
+    # train_img_cnt = len(open(train_file, 'r').readlines())
+    # val_img_cnt = len(open(val_file, 'r').readlines())
+    train_img , val_img = data_split(val_split,data_file,train_file,val_file)
--- test_single_image.py
+++ test_single_image.py
@@ -13,6 +13,10 @@ from utils.plot_utils import get_color_table, plot_one_box
 
 from model import yolov3
 
+import os
+from os import listdir, getcwd
+from os.path import join
+
 parser = argparse.ArgumentParser(description="YOLO-V3 test single image test procedure.")
 parser.add_argument("image_path", type=str,default="./Container/test_cdc/test_images",
                     help="The path of the input image.")
@@ -24,6 +28,11 @@ parser.add_argument("--class_name_path", type=str, default="./Container/containe
                     help="The path of the class names.")
 parser.add_argument("--restore_path", type=str, default="./Container/weight/container.ckpt",
                     help="The path of the weights to restore.")
+
+out_dirs = 'Container/DETECTImages/'
+submission = open("Container/Submission.csv","w") 
+submission.write('image_filename,label_id,x,y,w,h,confidence')
+submission.write("\n") 
 args = parser.parse_args()
 
 args.anchors = parse_anchors(args.anchor_path)
@@ -32,47 +41,58 @@ args.num_class = len(args.classes)
 
 color_table = get_color_table(args.num_class)
 
-img_ori = cv2.imread(args.image_path)
-height_ori, width_ori = img_ori.shape[:2]
-img = cv2.resize(img_ori, tuple(args.new_size))
-img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-img = np.asarray(img, np.float32)
-img = img[np.newaxis, :] / 255.
+for idx,f in enumerate(listdir(lists)):
+    img_path = lists + f
 
-with tf.Session() as sess:
-    input_data = tf.placeholder(tf.float32, [1, args.new_size[1], args.new_size[0], 3], name='input_data')
-    yolo_model = yolov3(args.num_class, args.anchors)
-    with tf.variable_scope('yolov3'):
-        pred_feature_maps = yolo_model.forward(input_data, False)
-    pred_boxes, pred_confs, pred_probs = yolo_model.predict(pred_feature_maps)
+    img_ori = cv2.imread(img_path)
+    height_ori, width_ori = img_ori.shape[:2]
+    img = cv2.resize(img_ori, tuple(args.new_size))
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = np.asarray(img, np.float32)
+    img = img[np.newaxis, :] / 255.
 
-    pred_scores = pred_confs * pred_probs
+    with tf.Session() as sess:
+        input_data = tf.placeholder(tf.float32, [1, args.new_size[1], args.new_size[0], 3], name='input_data')
+        yolo_model = yolov3(args.num_class, args.anchors)
+        with tf.variable_scope('yolov3'):
+            pred_feature_maps = yolo_model.forward(input_data, False)
+        pred_boxes, pred_confs, pred_probs = yolo_model.predict(pred_feature_maps)
 
-    boxes, scores, labels = gpu_nms(pred_boxes, pred_scores, args.num_class, max_boxes=30, score_thresh=0.4, nms_thresh=0.5)
+        pred_scores = pred_confs * pred_probs
 
-    saver = tf.train.Saver()
-    saver.restore(sess, args.restore_path)
+        boxes, scores, labels = gpu_nms(pred_boxes, pred_scores, args.num_class, max_boxes=30, score_thresh=0.4, nms_thresh=0.5)
 
-    boxes_, scores_, labels_ = sess.run([boxes, scores, labels], feed_dict={input_data: img})
+        saver = tf.train.Saver()
+        saver.restore(sess, args.restore_path)
 
-    # rescale the coordinates to the original image
-    boxes_[:, 0] *= (width_ori/float(args.new_size[0]))
-    boxes_[:, 2] *= (width_ori/float(args.new_size[0]))
-    boxes_[:, 1] *= (height_ori/float(args.new_size[1]))
-    boxes_[:, 3] *= (height_ori/float(args.new_size[1]))
+        boxes_, scores_, labels_ = sess.run([boxes, scores, labels], feed_dict={input_data: img})
 
-    print("box coords:")
-    print(boxes_)
-    print('*' * 30)
-    print("scores:")
-    print(scores_)
-    print('*' * 30)
-    print("labels:")
-    print(labels_)
+        # rescale the coordinates to the original image
+        boxes_[:, 0] *= (width_ori/float(args.new_size[0]))
+        boxes_[:, 2] *= (width_ori/float(args.new_size[0]))
+        boxes_[:, 1] *= (height_ori/float(args.new_size[1]))
+        boxes_[:, 3] *= (height_ori/float(args.new_size[1]))
 
-    for i in range(len(boxes_)):
-        x0, y0, x1, y1 = boxes_[i]
-        plot_one_box(img_ori, [x0, y0, x1, y1], label=args.classes[labels_[i]], color=color_table[labels_[i]])
-    cv2.imshow('Detection result', img_ori)
-    cv2.imwrite('detection_result.jpg', img_ori)
-    cv2.waitKey(0)
+        print("box coords:")
+        print(boxes_)
+        print('*' * 30)
+        print("scores:")
+        print(scores_)
+        print('*' * 30)
+        print("labels:")
+        print(labels_)
+
+        for i in range(len(boxes_)):
+            x0, y0, x1, y1 = boxes_[i]
+            plot_one_box(img_ori, [x0, y0, x1, y1], label=args.classes[labels_[i]], color=color_table[labels_[i]])
+            lines = [f ,int(labels_[i])+1,x0,y0,abs((x1-x0)),abs((y1-y0)) ,scores_ ]
+
+            for idx,r in enumerate(lines): 
+                submission.write(str(r))
+                if idx != (len(lines)-1):
+                    submission.write(",")
+            submission.write("\n")           
+
+    # cv2.imshow('Detection result', img_ori)
+    cv2.imwrite(out_dirs + f, img_ori)
+    # cv2.waitKey(0)
